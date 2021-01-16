3ec20610327614c093408495395989e7b3be2f43
Fatih Sahin <fatihsahin3@hotmail.com>
2021-01-16 22:12:58 +0100

    16/01/2021

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = "Merhaba Dunya";
+            string text2 = "Merhaba Mars";
+
+            Console.WriteLine(text1);
+            Console.WriteLine(text2);
+            Console.WriteLine("-----------------------------------");
+
+            text1 = text2;
+            Console.WriteLine(text1);
+            Console.WriteLine(text2);
+
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2};
+
+            foreach (Urun item in urunler)
+            {
+                Console.WriteLine("Urun Adi: " + item.Adi + "\nUrun Fiyati: " + item.Fiyati + "\nUrun Aciklamasi: " + item.Aciklama);
+
+            }
+
+            Console.WriteLine("------------Metotlar---------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12);
+
+        }
+    }
+}
